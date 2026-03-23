e18a2ffb07aed53c67d67ab7cef26b76ce7d505e
nonyx777 <nahomretebo@gmail.com>
2026-03-23 23:17:49 +0300

    EOD commit, started working on the solvers

--- Script/xpd.gd
+++ Script/xpd.gd
@@ -3,15 +3,52 @@ extends Node3D
 var tet_mesh: TetGenMesh
 var tet_array_constructor: TetArrayConstructor
 
-var verts: PackedVector3Array
-var edgeIds: PackedInt32Array
-var tetIds: PackedInt32Array
+var pos: PackedVector3Array
+var prev_pos: PackedVector3Array
+var inv_mass: PackedFloat32Array
+var velocity: PackedVector3Array
+
+var edge_Ids: PackedInt32Array
+var tet_Ids: PackedInt32Array
 
 var edge_stride: PackedInt32Array
 var edge_vertex_indices: PackedInt32Array
 
 var tet_stride: PackedInt32Array
 var tet_vertex_indices: PackedInt32Array
+
+var edge_compliance: float = 0.0
+var volume_compliance: float = 0.0
+
+var edge_lengths: PackedFloat32Array
+
+func preSolve(dt: float, force: Vector3):
+	for i in range(pos.size()):
+		if (inv_mass[i] == 0.0):
+			continue
+		velocity[i] += force * dt
+		prev_pos[i] = pos[i]
+		pos[i] += velocity[i] * dt
+		
+		if pos[i].y < 0.0:
+			pos[i] = prev_pos[i]
+			pos[i].y = 0.0;
+
+func solve(dt: float):
+	solveEdges(edge_compliance, dt)
+	solveVolumes(volume_compliance, dt)
+
+func solveEdges(compliance: float, dt: float):
+	pass
+func solveVolumes(compliance: float, dt: float):
+	pass
+
+func postSolve(dt):
+	for i in range(pos.size()):
+		if inv_mass[i] == 0.0:
+			continue
+		velocity[i] = (pos[i] - prev_pos[i]) * (1.0 / dt)
+	# update mesh
 
 func _ready():
 	tet_mesh = TetGenMesh.new()
@@ -35,9 +72,9 @@ func _ready():
 		print("Mesh added to scene. Bounds: ", bounds)
 		print("Mesh center: ", center)
 		
-		verts = tet_mesh.vertices
-		edgeIds = tet_array_constructor.construct_compact_edge(tet_mesh.edges, edge_stride, edge_vertex_indices)
-		tetIds = tet_array_constructor.construct_compact_tet(tet_mesh.tetrahedra, tet_stride, tet_vertex_indices)
+		pos = tet_mesh.vertices
+		edge_Ids = tet_array_constructor.construct_compact_edge(tet_mesh.edges, edge_stride, edge_vertex_indices)
+		tet_Ids = tet_array_constructor.construct_compact_tet(tet_mesh.tetrahedra, tet_stride, tet_vertex_indices)
 		
 		print("Edge Stride: ", edge_stride.size())
 		print("Edge Vertex Indices: ", edge_vertex_indices.size())
